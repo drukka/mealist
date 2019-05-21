97579e9369337cd76a9a7ccae4f6f1b054fc6570
Levente Dimény <levente.dimeny@drukka.hu>
2019-05-21 13:31:47 +0200

    Set SplashViewController as root

--- Mealist/Controllers/SplashViewController.swift
+++ Mealist/Controllers/SplashViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class SplashViewController: UIViewController {
 
+    // MARK: - Initialization
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     // MARK: - View life cycle
 
     override func viewDidLoad() {
