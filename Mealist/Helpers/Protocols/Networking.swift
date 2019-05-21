93db538da3a2d28706d5daa28ca3c8a5dea626a3
Németh Barna <barna.nemeth@drukka.hu>
2019-05-21 16:40:51 +0200

    Add Networking protocol and api client classes

---
+++ Mealist/Helpers/Protocols/Networking.swift
@@ -0,0 +1,13 @@
+//
+//  Networking.swift
+//  Mealist
+//
+//  Created by Németh Barna on 2019. 05. 21..
+//  Copyright © 2019. Levente Dimény. All rights reserved.
+//
+
+import Foundation
+
+protocol Networking {
+    
+}
---
+++ Mealist/Stores/FakeAPIClient.swift
@@ -0,0 +1,20 @@
+//
+//  FakeAPIClient.swift
+//  Mealist
+//
+//  Created by Németh Barna on 2019. 05. 21..
+//  Copyright © 2019. Levente Dimény. All rights reserved.
+//
+
+import Foundation
+
+class FakeAPIClient: Networking {
+    
+    // MARK: - Properties
+    
+    private let mockUser = UserData(id: 1, email: "mock@test.com", firstname: "Mock", lastname: "Test", profilePicture: nil, deliveryDetails: nil)
+    
+    // MARK: - Public methods
+    
+    
+}
