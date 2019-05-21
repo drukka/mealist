6edab9e3c0ce54737aabfa8bb7aeb1b19c485459
Levente Dimény <levente.dimeny@drukka.hu>
2019-05-21 13:53:42 +0200

    Create SignUpViewController and associated xib file

---
+++ Mealist/Controllers/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  Mealist
+//
+//  Created by Levente Dimény on 2019. 05. 21..
+//  Copyright © 2019. Levente Dimény. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
