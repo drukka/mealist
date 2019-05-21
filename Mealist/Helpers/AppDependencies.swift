9298f3fb952fe672d7414e31d9c90d8257f5807a
Németh Barna <barna.nemeth@drukka.hu>
2019-05-21 16:45:12 +0200

    Add AppDependecies

---
+++ Mealist/Helpers/AppDependencies.swift
@@ -0,0 +1,31 @@
+//
+//  AppDependencies.swift
+//  Mealist
+//
+//  Created by Németh Barna on 2019. 05. 21..
+//  Copyright © 2019. Levente Dimény. All rights reserved.
+//
+
+import Foundation
+import Swinject
+
+final class AppDependencies {
+    
+    // MARK: - Public methods
+    
+    func setup(with container: Container) {
+        self.registerNetworking(to: container)
+    }
+    
+    // MARK: - Private methods
+    
+    private func registerNetworking(to container: Container) {
+        container.register(Networking.self, factory: { _ in
+            #if MOCK
+                return FakeAPIClient()
+            #else
+                return APIClient()
+            #endif
+        })
+    }
+}
